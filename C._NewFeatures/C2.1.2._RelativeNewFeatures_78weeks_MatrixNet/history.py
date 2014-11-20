b160c05b05e739551c3b84624b18f7c6fc0252fe
ipykee <ipykee@fake.email>
2014-11-20 14:45:52 +0000

    C2.1.2._RelativeNewFeatures_78weeks_MatrixNet: Upload by ipykee. Second commit.

--- C._NewFeatures/C2.1.2._RelativeNewFeatures_78weeks_MatrixNet/history.py
+++ C._NewFeatures/C2.1.2._RelativeNewFeatures_78weeks_MatrixNet/history.py
@@ -7,13 +7,3 @@ import ipykee
 #ipykee.create_project("C._NewFeatures", internal_path="C._NewFeatures", repository="git@github.com:hushchyn-mikhail/CERN_Time_Series.git")
 session = ipykee.Session(project_name="C._NewFeatures")
 
-# <codecell>
-
-session.commit("Upload by ipykee. First commit.")
-
-# <codecell>
-
-import ipykee
-ipykee.create_project("C._NewFeatures", internal_path="C._NewFeatures", repository="git@github.com:hushchyn-mikhail/CERN_Time_Series.git")
-session = ipykee.Session(project_name="C._NewFeatures")
-
--- C._NewFeatures/C2.1.2._RelativeNewFeatures_78weeks_MatrixNet/history_manual.py
+++ C._NewFeatures/C2.1.2._RelativeNewFeatures_78weeks_MatrixNet/history_manual.py
@@ -9,14 +9,4 @@ session = ipykee.Session(project_name="C._NewFeatures")
 
 # <codecell>
 
-session.commit("Upload by ipykee. First commit.")
-
-# <codecell>
-
-import ipykee
-ipykee.create_project("C._NewFeatures", internal_path="C._NewFeatures", repository="git@github.com:hushchyn-mikhail/CERN_Time_Series.git")
-session = ipykee.Session(project_name="C._NewFeatures")
-
-# <codecell>
-
-session.commit("Upload by ipykee. First commit.")
+session.commit("Upload by ipykee. Second commit.")
