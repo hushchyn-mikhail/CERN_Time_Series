610f7e685fe0caf97628df5aa850a8368df13f46
ipykee <ipykee@fake.email>
2014-11-20 14:38:31 +0000

    C2.1.2._RelativeNewFeatures_78weeks_MatrixNet: Upload by ipykee. First commit.

---
+++ C._NewFeatures/C2.1.2._RelativeNewFeatures_78weeks_MatrixNet/history.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import ipykee
+#ipykee.create_project("C._NewFeatures", internal_path="C._NewFeatures", repository="git@github.com:hushchyn-mikhail/CERN_Time_Series.git")
+session = ipykee.Session(project_name="C._NewFeatures")
+
+# <codecell>
+
+session.commit("Upload by ipykee. First commit.")
+
+# <codecell>
+
+import ipykee
+ipykee.create_project("C._NewFeatures", internal_path="C._NewFeatures", repository="git@github.com:hushchyn-mikhail/CERN_Time_Series.git")
+session = ipykee.Session(project_name="C._NewFeatures")
+
